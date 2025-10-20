7aaf5691c44ea7c67441d3d5aef66fb7abbf74fd
rfatim02 <rfatim02@uoguelph.ca>
2025-10-20 13:22:55 -0400

    Update word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -24,3 +24,45 @@ def is_sentence(text):
         return False
 
     return True
+def ask_user():
+    user_sentence = input("Enter a sentence: ")
+    while (is_sentence(user_sentence) == False):
+        print("This does not meet the criteria for a sentence.")
+        user_sentence = input("Enter a sentence: ")
+
+    return user_sentence
+        
+def replace_letter(user_sentence):
+    user_sentence = user_sentence.replace('.', '')
+    user_sentence = user_sentence.replace('!', '')
+    user_sentence = user_sentence.replace('?', '')
+
+    user_sentence = user_sentence.lower()
+    word_list = user_sentence.split()
+    return word_list
+
+def frequency_count(word_list):
+    words = []
+    frequencies = []
+
+    for i in word_list:
+        if words.count(i) > 0:
+            index = words.index(i)
+            frequencies[index] = frequencies[index] + 1
+        else:
+            words.append(i)
+            frequencies.append(1)
+    return [words,frequencies]
+    
+def print_output(words, frequencies):
+    print("Output:")
+    for i in range(len(words)):
+        print(words[i], ":", frequencies[i])
+
+def main():
+    user_sentence = ask_user()
+    word_list = replace_letter(user_sentence)
+    words, frequencies = frequency_count(word_list)
+    print_output(words, frequencies)
+    
+main()
